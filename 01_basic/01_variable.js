40a37168957c817949411b5cf5d75c9f51401255
sam3990 <39296046+sam3990@users.noreply.github.com>
2024-08-20 19:56:55 +0000

    datatype

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accountId = 12
+let accountEmail = "sameer@gmail.com"
+var accountPswd = "121212"
+
+console.log(accountId)
+//accountId = 2 //not allowed
+
+console.table([accountId,accountEmail,accountPswd])
---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+//alert(3+3) // we are using node.js, not browser
+
+let name = "sameer" //string
+let age = 18 // number
+let isLoggedIn = true // boolean
+
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value null is object type 
+//symbol =>unique
+
+//object
+
+console.log(typeof age) // to get data type of variable use typeof
+console.log(typeof null) // object
+console.log(typeof undefined) //undefined
