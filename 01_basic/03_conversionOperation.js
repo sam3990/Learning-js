7ed97bcb9e626a40d309dfc17f2aa988e9b59bc6
sam3990 <39296046+sam3990@users.noreply.github.com>
2024-08-21 18:47:11 +0000

    ff

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33a"
+let valueInNumber = Number(score)
+
+//console.log (valueInNumber)
+// Number Conversion
+//  33 => 33
+// "33" => 33
+// "33a" => NaN
+// Null => 0
+//Undefined => NaN
+
+let logIn = "sameer"
+let logInBool = Boolean(logIn)
+
+
+//console.log (logInBool)
+
+// boolean Conversion
+ // 1 => true
+ //0 => false
+ // "" => false
+ // "sameer" => true 
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ //console.log (stringNumber)
+
+
+ //*********************Operation*****************
+ // console.log(2%3)
